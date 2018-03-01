3921083010b569faa429879608216d22201ae5d2
Managersky <witoldmetel@gmail.com>
2018-03-01 19:22:32 +0100

    add print functionality

--- js/printContent.js
+++ js/printContent.js
@@ -1,11 +1,41 @@
-const buttonPrint = document.getElementById("print");
 
-buttonPrint.onclick = function() {
-    let restorePage = document.body.innerHTML;
-    let printContent = document.getElementsByClassName("ittinery-box")[0].innerHTML;
-    let popup = window.open("", "popup");
+//My Script in Vanilla JS (Working without CSS what's mean you can print but without styliing)
 
-    popup.document.body.innerHTML = printContent;
-    popup.focus();
-    popup.print();
-}
+// const buttonPrint = document.getElementById("print");
+
+// buttonPrint.onclick = function() {
+//     let restorePage = document.body.innerHTML;
+//     let printContent = document.getElementsByClassName("ittinery-box")[0].innerHTML;
+//     let popup = window.open("", "popup");
+
+//     popup.document.body.innerHTML = printContent;
+//     popup.focus();
+//     popup.print();
+// }
+
+// Script based on jsPDF plugin / html2canvas
+
+
+
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    // source can be HTML-formatted string, or a reference
+    // to an actual DOM element from which the text will be scraped.
+    source = $('content-to-print')[0];
+
+    // we support special element handlers. Register them with jQuery-style
+    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+    // There is no support for any other type of selectors
+    // (class, of compound) at this time.
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '.calendar': function (element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true;
+        }
+    };
+
+$('#print').click(function () {
+pdf.addHTML($('#content-to-print')[0], function () {
+    pdf.save('My-ittinery.pdf');
+});
+});
--- js/sortDestination.js
+++ js/sortDestination.js
@@ -12,3 +12,17 @@ $(".sort-destination").on("click", function (e) {
     }).show();
 
 });
+
+$.expr[':'].contains = function (a, i, m) { //insensitive version of the :contains() (:constains() - Select all elements that contain the specified text)
+    return $(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+$('#search').keyup(function () {
+    let searchValue = this.value;
+    searchValue = searchValue.split(" ");
+    $('#destination-choose-list > div').hide();
+    $.each(searchValue, function (i) { //Split the search string and parse using .each (.each - Iterate over a jQuery object, executing a function for each matched element)
+        $('#destination-choose-list > div:contains("' + searchValue[i] + '")').show();
+    });
+});
